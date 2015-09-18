b56d659ad45a4f7047cf6915ac4165ae48f59862
phimyndtec <phi@myndtec.com>
2015-09-18 14:29:21 -0400

    test

---
+++ testSVNPlug-in source control/testSVNPlug-in source control/src/Calculate.c
@@ -0,0 +1,15 @@
+/*
+ * Calculate.c
+ *
+ * Created: 9/18/2015 12:05:05 PM
+ *  Author: Phi
+ */ 
+#include "calculate.h"
+test()
+{
+	int a = 5;
+	int b = 10;
+	int c = a+b;
+	
+	// test here
+}
---
+++ testSVNPlug-in source control/testSVNPlug-in source control/src/calculate.h
@@ -0,0 +1,19 @@
+/*
+ * main.h
+ *
+ * Created: 9/18/2015 11:48:56 AM
+ *  Author: Phi
+ */ 
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+void test(void);
+
+
+#endif /* MAIN_H_ */
